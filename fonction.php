50dcf7d6a3b6ba88fd57b0370fada5ee35f87c36
Jazzjut <151069301+Jazzjut@users.noreply.github.com>
2024-01-18 17:42:35 +0100

    Create fonction.php

---
+++ fonction.php
@@ -0,0 +1,12 @@
+<?php
+function executeRequete($req)
+{
+    global $mysqli;
+    $resultat = $mysqli->query($req);
+    if(!$resultat) // 
+    {
+        die("Erreur sur la requete sql.<br>Message : " . $mysqli->error . "<br>Code: " . $req);
+    }
+    return $resultat; // 
+}
+?>
